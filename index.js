13d6ad17faed9b147331b0760c65e586138f5088
Damian K <damian-8-8@o2.pl>
2019-09-18 00:35:22 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+const path = require('path');
+const {app, BrowserWindow} = electron;
+
+global.sharedObject = {}
+
+app.on('ready', () => {
+	const screenElectron = electron.screen.getPrimaryDisplay();
+	const optimalWidth = screenElectron.size.width * 9/10;
+	const optimalHeight = screenElectron.size.height * 9/10;
+
+	const win = new BrowserWindow(
+		{
+			backgroundColor: '#ffffff',
+			width: optimalWidth,
+			minWidth: 1240,
+			height: optimalHeight,
+			minHeight: 800,
+			center: true,
+			transparent: true,
+			frame: false,
+			icon: path.join(__dirname, 'icon.png'),
+			webPreferences: {
+				nodeIntegration: true
+			}
+		}
+	);
+	win.loadURL("file://" + __dirname + "/src/main.html");
+})
